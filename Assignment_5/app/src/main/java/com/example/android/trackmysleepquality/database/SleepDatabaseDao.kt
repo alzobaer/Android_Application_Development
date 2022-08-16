0b68d5047dee0baae6016debc67d0b57bce9c16f
Al Zobaer <al.zobaer@yahoo.com>
2022-08-16 16:08:45 +0600

    Exercise-7 is added to Assignment5

--- Assignment_5/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ Assignment_5/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -13,7 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.example.android.trackmysleepquality.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface SleepDatabaseDao{
+
+    @Insert
+    fun  insert(night: SleepNight)
+
+    @Update
+    fun update(night: SleepNight)
+
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    fun get(key: Long): SleepNight
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAlNights(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight
+
+}
